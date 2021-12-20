91cc56b3d3bb579343d5913be50d03b13a5605bd
STUMPF1988 <stumpfdaniele@gmail.com>
2021-12-19 21:27:36 -0300

    segundo_commit

--- public/api.js
+++ public/api.js
@@ -3,12 +3,11 @@ const api = axios.create({
   baseURL: "https://api-curriculo-stumpf1988.herokuapp.com/",
 });
 
-//post
-function Enviar() {
+//post cadastro
+function EnviaCadastro() {
   const nome = document.querySelector("#nome").value;
   const telefone = document.querySelector("#telefone").value;
   const email = document.querySelector("#email").value;
-  const comentario = document.querySelector("#comentario").value;
 
   if (nome == "" || telefone == "" || email == "") {
     alert("Informe todos os dados!");
@@ -21,11 +20,40 @@ function Enviar() {
       nome,
       telefone,
       email,
-      comentario,
     })
     .then((result) => {
       const cadastro = result.data;
       cadastroList = cadastro;
+      window.location.href = "index.html";
+    })
+    .catch((err) => {
+      alert(err);
+      console.log(err);
+      console.log(err.request);
+      console.log(err.response);
+    });
+}
+
+
+//post comentario
+function EnviaComentario() {
+  const nome2 = document.querySelector("#nome2").value;
+  const comentario = document.querySelector("#comentario").value;
+
+  if (nome2 == "" || comentario == "") {
+    alert("Informe todos os dados!");
+  } else {
+    alert("Mensagem enviada com sucesso!");
+  }
+
+  api
+    .post("/", {
+      nome2,
+      comentario,
+    })
+    .then((result) => {
+      const coment = result.data;
+      comentarioList = coment;
       window.location.href = "index.html";
     })
     .catch((err) => {
